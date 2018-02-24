48d7fba0edda845b17442f340205305f71fd519a
Kevin Chen <kchen42@stanford.edu>
2018-02-23 22:30:17 -0800

    Save file

--- project.py
+++ project.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import numpy.random as rd
 import time
+import sys
+from datetime import timedelta
+from scipy.stats import norm
 
 rd.seed(seed = 0)
 
@@ -16,11 +19,12 @@ Parameters
 
 # Input
 n_MC = 10000
-n = 100
+n = 1000
 n_t = 100
 T_range = np.array([1, 3, 5])
 mu_range = 0.01 * np.arange(1, 21)
 beta_scale_factor = 10
+CI_perc = 95
 
 # Model parameters
 kappa = rd.uniform(0.5, 1.5, n)
@@ -31,10 +35,36 @@ gamma = np.sqrt(np.square(kappa) + 2 * np.square(sigma))
 beta = rd.uniform(0, 0.01, (n, n)) / beta_scale_factor
 np.fill_diagonal(beta, 0)
 
-# Choose input
-T = T_range[0]
-mu = mu_range[-1]
+
+'''
+Usage
+'''
+
+
+# Error message
+if len(sys.argv) < 3:
+    print "Usage:"
+    print "  $python {0} <T> <mu>".format(sys.argv[0])
+    print "   <T> in {1, 3, 5}"
+    print "  <mu> in {0.01, 0.02, ..., 0.20}"
+    sys.exit()
+
+# Get input    
+T = int(sys.argv[1])
+mu = float(sys.argv[2])
+if (not (T in T_range)) or (not (mu in mu_range)):
+    print "Usage:"
+    print "  $python {0} <T> <mu>".format(sys.argv[0])
+    print "   <T> in {1, 3, 5}"
+    print "  <mu> in {0.01, 0.02, ..., 0.20}"
+    sys.exit()
+
+    
+# Related coefficients
+mu = 0.01
+T = 1
 theta = np.ceil(mu*n) / T
+CI_coef = norm.ppf(0.5 + CI_perc/200.0)
 
 
 '''
@@ -62,18 +92,25 @@ Importance sampling
 
 # Store variables
 Y_total = np.zeros(n_MC)
+m_MC = rd.poisson(np.ceil(mu*n), n_MC)
 t_start = time.time()
 
 # Do Monte Carlo IS
 for i_MC in range(n_MC):
 
-    # Step 1 - Generate S
-    # TODO - change S generation scheme
+    # Print progress
+#    print i_MC
+    if (i_MC+1) % (n_MC/100) == 0:
+        print "{0: >3}% done in {1}".format(
+                int(100*(i_MC+1)/n_MC), timedelta(seconds = time.time()-t_start))
     
-    S = np.cumsum(rd.exponential(scale = 1/theta,      # beta = 1/lambda
-                                 size = int(5*theta))) # E[N] = lambda, x5 to make sure
-    S = S[S <= T][:n]
-    m = int(S.shape[0])
+    # Step 1 - Generate S
+    
+    m = m_MC[i_MC]
+    # Go to the next iteration if no default
+    if m == 0:
+        continue
+    S = np.sort(rd.uniform(0.0, T, m))
     p_wob = p_nocorr(S, kappa, X0, c, gamma)
     
     # Step 2 - Draw I and update M
@@ -116,17 +153,27 @@ for i_MC in range(n_MC):
     Y = Z * (m >= mu*n)
     Y_total[i_MC] = Y
     
-    # Print progress
-    if (i_MC+1) % (n_MC/20) == 0:
-        print "Iteration {0}/{1} in {2}s".format(i_MC+1, n_MC, 
-                                                 time.time() - t_start)
-    
 
 '''
 Results
 '''
 
+
 print "mu*n = {0}".format(mu*n) 
 print "IS Estimate = {0}".format(np.mean(Y_total))
 print "IS Variance = {0}".format(np.var(Y_total))
-    
+
+t_end = time.time()
+t_used = t_end - t_start
+filename = "IS_{0}_{1:.2f}.txt".format(T, mu)
+with open(filename, 'w') as f:
+    f.write("n_MC = {0:.0f}\n".format(n_MC))
+    f.write("n = {0:.0f}\n".format(n))
+    f.write("T = {0:.0f}\n".format(T))
+    f.write("mu = {0:.2f}\n".format(mu))
+    f.write("mu*n = {0:.0f}\n".format(mu*n))
+    f.write("IS Estimate = {0:e}\n".format(np.mean(Y_total)))
+    f.write("IS Variance = {0:e}\n".format(np.var(Y_total)))
+    f.write("{0}% CI = {1:e}\n".format(CI_perc,
+                                       CI_coef*np.sqrt(np.var(Y_total)/n_MC)))
+    f.write("Time spent = {0}\n".format(timedelta(seconds = t_used)))
