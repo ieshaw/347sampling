b9d89fd1dc9d0e42039a5fa5b4a7d23db8d0f16b
Ian Shaw <ianedwardshaw@gmail.com>
2018-03-22 16:34:59 -0700

    New Python Scripts

--- new_csv_to_latex.py
+++ new_csv_to_latex.py
@@ -8,7 +8,7 @@ mc_df = pd.read_csv(file_path +'/MC_100.csv', index_col=0)
 
 is_df = pd.read_csv(file_path +'/IS_new.csv', index_col=0)
 
-out_file = file_path + '/paper_new.tex'
+out_file = os.path.abspath(os.path.dirname(__file__)) + '/new_stuff/paper_new.csv'
 
 mc_df['fake_index'] = mc_df['T'].astype(str) + '_' + mc_df['mu'].astype(str)
 
@@ -30,4 +30,4 @@ out_df = out_df.reset_index(drop= True)
 
 out_df.to_csv(out_file, index=False)
 
-print out_df.to_latex(index = False)
+#out_df.to_latex(out_file, index = False)
